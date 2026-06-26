4f0cb25da6929833aa636c4e83e599846ad83b17
syafiq <fathazwan14@gmail.com>
2026-06-26 18:50:01 +0800

    fix: bootstrap laravel kernel in helper script

--- public/cpanel-helper.php
+++ public/cpanel-helper.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\DB;
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
 echo "<h2>Portfolio cPanel Helper & Setup</h2>";
 
